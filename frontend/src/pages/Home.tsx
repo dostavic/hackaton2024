da126c8ab460d6330c5f39e86a09cf42f26be8ab
Ruslan <dyakovruslan066@gmail.com>
2024-11-23 16:08:03 +0100

    Initialize frontend

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,18 @@
+// src/pages/Home.tsx
+
+import React from 'react';
+
+const Home: React.FC = () => {
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <header className="mb-8">
+        <h1 className="text-4xl font-bold">Добро пожаловать!</h1>
+      </header>
+
+      
+    </div>
+  );
+};
+
+export default Home;
